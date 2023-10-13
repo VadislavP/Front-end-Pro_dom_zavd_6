cbabcce1438483f8eebb248c441a56fd16308a11
VladislavP <VladislavP@ukr.net>
2023-10-13 22:30:24 +0300

    2 zavdanna

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ function removeElement(array, item) {
 removeElement(array, 4);
 
 
+const myArray = [1, 2, 'three', 4, 'five', 6];
+function calculateAverage(myArray) {
+    let sum = 0;
+    let quantityInteger = 0;
+    // let quantity = myArray.length
+    let res = myArray.reduce(function (sum1, item){ /*відпрацювання методу reduce*/
+        if (typeof item === 'number') {
+        return sum1 + item;
+        } else {
+        return sum1}
+        })
+    console.log('res=', res)
+    let sum2 = 0;
+    for (let i = 0; i < myArray.length; i++) 
+        if (!isNaN(myArray[i])) {            
+            quantityInteger += 1;
+            sum2 += myArray[i];            
+        }
+    // середнє аріфметичнe average1, де res отримано за допомогою методу reduce
+    let average1 = res / quantityInteger 
+    // середнє аріфметичнe average2, де sum2 отримано за допомогою циклу з умовою рахування чисел з виключенням строк
+    let average2 = sum2 / quantityInteger
+    console.log('sum2=', sum2)
+    console.log('quantityInteger=', quantityInteger)
+    console.log('average1=', average1)
+    console.log('average2=', average2)
+    return average1
+
+}
+const average = calculateAverage(myArray);
+alert(`Середнє аріфметичне функції calculateAverage = ${average}`)
+
+
+
+// console.log(myArray.length)
+// console.log(`Середнє арифметичне чисел в масиві: ${average}`);
 
 // Array.prototype.remove = function (value) {
 //     this.splice(this.indexOf(value), 1);
